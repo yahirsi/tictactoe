ed215dcad0c31f7477815ccc319cb629011f9106
Glitch (imported-somber-wolf) <none>
2021-02-10 02:54:35 +0000

    💷🏨 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,43 +1,86 @@
-
-
-
-
-
+var currentUser = "O";
+var numberTurns = 0;
+var endGame = "false";
+var board = [null, null, null, null, null, null, null, null, null];
+Window.board = board;
+function performLogic(BID, TID) {
+  $(TID).html(currentUser);
+  changePlayer();  
+}
+function changePlayer(){
+  if (currentUser == "O"){
+    currentUser = "X";
+  } else{
+    currentUser = "O";
+  }
+}
+function playerWins(tile1, tile2, tile3){
+  if (tile1 == currentUser && tile2 == currentUser && tile3 == currentUser){
+    return currentUser + "Wins";
+  }
+}
+function verticleTiles(){
+  
+  playerWins("#tile1, #tile4, #tile7")
+  playerWins("#tile2, #tile5, #tile8")
+  playerWins("#tile3,#tile6, #tile9")
+}
+  
+function performLogic(tileId) {
+  let button = $("#button" + tileId);
+  writeSymbol(tileId);
+  checkBoard();
+  changePlayer();
+  
+  console.log(board);
+}
+    
+function writeSymbol(tileId) {
+  let tile = $("#tile" + tileId);
+  tile.html(currentUser);
+  board[tileId - 1] = currentUser;
+}   
+function checkBoard(){
+  
+}
+function getRow(n) {
+  return [board[n - 1], board[n], board[n + 1]];
+}
 
 
 $("#button1").click(function() {
-    performLogic("#button1","#tile1");
+  performLogic(1);
+  console.log("Hi")
 });
 
 $("#button2").click(function() {
-    performLogic("#button2","#tile2");
+  performLogic(2);
 });
 
 $("#button3").click(function() {
-    performLogic("#button3","#tile3");
+  performLogic(3);
 });
 
 $("#button4").click(function() {
-    performLogic("#button4","#tile4");
+  performLogic(4);
 });
 
 $("#button5").click(function() {
-    performLogic("#button5","#tile5");
+  performLogic(5);
 });
 
 $("#button6").click(function() {
-    performLogic("#button6","#tile6");
+  performLogic(6);
 });
 
 $("#button7").click(function() {
-    performLogic("#button7","#tile7");
+  performLogic(7);
 });
 
 $("#button8").click(function() {
-    performLogic("#button8","#tile8");
+  performLogic(8);
 });
 
 $("#button9").click(function() {
-    performLogic("#button9","#tile9");
+  performLogic(9);
 });
-
